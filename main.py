3287f112074dc80a9508ff2a40fd99eddbf4f37b
David Antaki <antakidavid@gmail.com>
2022-06-26 23:47:25 -0400

    Delete dead code

--- main.py
+++ main.py
@@ -213,7 +213,6 @@ scaler = preprocessing.StandardScaler().fit(trainset_x)
 trainset_x = scaler.transform(trainset_x)
 scaler = preprocessing.StandardScaler().fit(testset_x)
 testset_x = scaler.transform(testset_x)
-# # trainset_x = preprocessing.normalize(trainset_x, norm='l2')
 # plt.scatter(np.arange(0, len(trainset_x)), trainset_x[:, 0])
 # plt.show()
 # trainset_x_df = pandas.DataFrame(trainset_x, columns=features[0:18])
@@ -244,21 +243,6 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 criterion = nn.CrossEntropyLoss()
 num_epochs = 1000
 
-# Convert numpy structures to PyTorch tensors, as these are the data types required by the library
-# trainset_x_tensor = torch.FloatTensor(trainset_x).to(torch.device('cuda'))
-# trainset_y_tensor = torch.LongTensor(trainset_y).to(torch.device('cuda'))
-
-# means = trainset_x_tensor.mean(dim=1, keepdim=True)
-# stds = trainset_x_tensor.std(dim=1, keepdim=True)
-# normalized_data = (trainset_x_tensor - means) / stds
-
-# testset_x_tensor = torch.FloatTensor(testset_x).to(torch.device('cuda'))
-# testset_y_tensor = torch.LongTensor(testset_y).to(torch.device('cuda'))
-
-# means = trainset_x_tensor.mean(dim=1, keepdim=True)
-# stds = trainset_x_tensor.std(dim=1, keepdim=True)
-# normalized_data = (trainset_x_tensor - means) / stds
-
 # trainset_x_df = pandas.DataFrame(normalized_data.detach().numpy(), columns=features[0:18])
 # sns.pairplot(data=trainset_x_df)
 # plt.scatter(trainset_x[:,0], np.arange(0, len(trainset_x)))
@@ -282,120 +266,3 @@ testset_y_pred_df = pandas.DataFrame(testset_y_pred, columns=['satisfaction'])
 sns.countplot(x='satisfaction', data=testset_y_pred_df)
 plt.title("Number of samples classified as Satisfied vs Unsatisfied")
 plt.show()
-
-'''
-class TwoLayerMLP(nn.Module):
-    # Two-layer MLP (not really a perceptron activation function...) network class
-    
-    def __init__(self, input_dim, hidden_dim, C):
-        super(TwoLayerMLP, self).__init__()
-        # Fully connected layer WX + b mapping from input_dim (n) -> hidden_layer_dim
-        self.input_fc = nn.Linear(input_dim, hidden_dim)
-        # Output layer again fully connected mapping from hidden_layer_dim -> outputs_dim (C)
-        self.output_fc = nn.Linear(hidden_dim, C)
-        
-    # Don't call this function directly!! 
-    # Simply pass input to model and forward(input) returns output, e.g. model(X)
-    def forward(self, X):
-        # X = [batch_size, input_dim (n)]
-        X = self.input_fc(X)
-        # Non-linear activation function, e.g. ReLU (default good choice)
-        # Could also choose F.softplus(x) for smooth-ReLU, empirically worse than ReLU
-        X = F.relu(X)
-        # X = [batch_size, hidden_dim]
-        # Connect to last layer and output 'logits'
-        y = self.output_fc(X)
-        return y
-
-    
-# input_dim = X.shape[1]
-# n_hidden_neurons = 16
-# output_dim = C
-
-# It's called an MLP but really it's not...
-model = TwoLayerMLP(input_dim, n_hidden_neurons, output_dim)
-# Visualize network architecture
-print(model)
-
-def model_train(model, data, labels, criterion, optimizer, num_epochs=25):
-    # Apparently good practice to set this "flag" too before training
-    # Does things like make sure Dropout layers are active, gradients are updated, etc.
-    # Probably not a big deal for our toy network, but still worth developing good practice
-    model.train()
-    # Optimize the neural network
-    for epoch in range(num_epochs):
-        # These outputs represent the model's predicted probabilities for each class. 
-        outputs = model(data)
-        # Criterion computes the cross entropy loss between input and target
-        loss = criterion(outputs, labels)
-        # Set gradient buffers to zero explicitly before backprop
-        optimizer.zero_grad()
-        # Backward pass to compute the gradients through the network
-        loss.backward()
-        # GD step update
-        optimizer.step()
-        
-    return model
-
-
-# Stochastic GD with learning rate and momentum hyperparameters
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-# The nn.CrossEntropyLoss() loss function automatically performs a log_softmax() to 
-# the output when validating, on top of calculating the negative log-likelihood using 
-# nn.NLLLoss(), while also being more stable numerically... So don't implement from scratch
-criterion = nn.CrossEntropyLoss()
-num_epochs = 100
-
-# Convert numpy structures to PyTorch tensors, as these are the data types required by the library
-X_tensor = torch.FloatTensor(trainset_x)
-y_tensor = torch.LongTensor(trainset_y)
-
-# Trained model
-model = model_train(model, X_tensor, y_tensor, criterion, optimizer, num_epochs=num_epochs)
-
-def model_predict(model, data):
-    # Similar idea to model.train(), set a flag to let network know your in "inference" mode
-    model.eval()
-    # Disabling gradient calculation is useful for inference, only forward pass!!
-    with torch.no_grad():
-        # Evaluate nn on test data and compare to true labels
-        predicted_labels = model(data)
-        # Back to numpy
-        predicted_labels = predicted_labels.detach().numpy()
-
-        return np.argmax(predicted_labels, 1)
-
-testset_x_tensor = torch.FloatTensor(testset_x)
-testset_y_tensor = torch.LongTensor(testset_y)
-
-testset_y_pred = af.model_predict(model, testset_x_tensor)
-# print("testset_y_pred:\n{}".format(np.unique(testset_y_pred)))
-# print("testset_y:\np{}".format(np.unique(testset_y)))
-print(testset_y_pred.shape)
-print(testset_y.shape)
-
-print(confusion_matrix(testset_y, testset_y_pred))
-testset_y_pred_df = pandas.DataFrame(testset_y_pred, columns=['satisfaction'])
-sns.countplot(x='satisfaction', data=testset_y_pred_df)
-plt.title("Number of samples classified as Satisfied vs Unsatisfied")
-plt.show()
-
-# # Set up test data as tensor
-# grid_tensor = torch.FloatTensor(grid)   
-# # Z matrix are the predictions resulting from the forward pass through the network
-# Z = model_predict(model, grid_tensor).reshape(xx.shape)
-
-# plt.figure(figsize=(10,8))
-
-# # uses gray background for black dots
-# plt.pcolormesh(xx, yy, Z, cmap=plt.cm.coolwarm)
-    
-# plt.plot(X[y==0, 0], X[y==0, 1], 'bx', label="Class 0")
-# plt.plot(X[y==1, 0], X[y==1, 1], 'ko', label="Class 1");
-# plt.plot(X[y==2, 0], X[y==2, 1], 'r*', label="Class 2");
-# plt.xlabel(r"$x_1$")
-# plt.ylabel(r"$x_2$")
-# plt.title("MLP Classification Boundaries Train Set")
-# plt.legend()
-# plt.show()
-'''
